8220ae800543b5f058554b747f810f071f65ec42
gleekzorp <gleek.dan@gmail.com>
2019-02-19 11:12:47 -0700

    Introduction to React Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Daniel Floyd's Portfolio</h1>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <PortfolioContainer />
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
       </div>
     );
   }
