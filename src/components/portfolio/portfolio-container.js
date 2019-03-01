bbde0b370fb062b08d12cd2221966a4347d6a380
gleekzorp <gleek.dan@gmail.com>
2019-03-01 14:32:16 -0700

    How to Create a Main Scss File to Import and Organize Styles in React

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,67 +1,73 @@
-import React, { Component } from "react"
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portfolio",
-            isLoading: false,
-            data: []
-        };
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
 
-        this.handleFilter = this.handleFilter.bind(this);
-    }
+    this.handleFilter = this.handleFilter.bind(this);
+  }
 
-    handleFilter(filter) {
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://danielfloyd.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
-    }
-
-    getPortfiolioItems() {
-        axios.get('https://danielfloyd.devcamp.space/portfolio/portfolio_items')
-          .then(response => {
-            this.setState({
-                data: response.data.portfolio_items
-            })
-        })
-          .catch(error => {
-          console.log(error);
+          data: response.data.portfolio_items
         });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return <PortfolioItem key={item.id} item={item} />;
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
 
-    portfolioItems() {
-        return this.state.data.map(item => {
-            return <PortfolioItem key={item.id} item={item} />;
-        })
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    componentDidMount() {
-        this.getPortfiolioItems();
-    }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>;
-        }
-
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-                {this.portfolioItems()}
-
-            </div>
-        );
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
