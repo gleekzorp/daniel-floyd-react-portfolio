3e08bf0bb367fcba1e00bf30a65afc0a4a6657ec
gleekzorp <gleek.dan@gmail.com>
2019-03-01 16:10:14 -0700

    Cleaning Up the App Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import moment from "moment";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -19,8 +18,6 @@ export default class App extends Component {
       <div className='app'>
         <Router>
           <div>
-            <h1>Daniel Floyds Portfolio</h1>
-            <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
             <NavigationContainer />
 
             <Switch>
