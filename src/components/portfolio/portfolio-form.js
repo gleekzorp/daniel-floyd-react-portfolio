239ad0c09387acea1f4e9984f5d1acd9d70c24e2
gleekzorp <gleek.dan@gmail.com>
2019-03-08 18:11:30 -0700

    Portfolio Manager Build Out.  Styles Complete

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -126,11 +126,8 @@ export default class PortfolioForm extends Component {
 
     render() {
         return (
-            <div>
-                <h1>PortfolioForm</h1>
-
-                <form onSubmit={this.handleSubmit}>
-                    <div>
+                <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
+                    <div className="two-column">
                         <input
                             type="text"
                             name="name"
@@ -148,7 +145,7 @@ export default class PortfolioForm extends Component {
                         />
                     </div>
 
-                    <div>
+                    <div className="two-column">
                         <input
                             type="text"
                             name="position"
@@ -161,6 +158,7 @@ export default class PortfolioForm extends Component {
                             name="category"
                             value={this.state.category}
                             onChange={this.handleChange}
+                            className="select-element"
                         >
                             <option value="eCommerce">eCommerce</option>
                             <option value="Scheduling">Scheduling</option>
@@ -168,7 +166,7 @@ export default class PortfolioForm extends Component {
                         </select>
                     </div>
 
-                    <div>
+                    <div className="one-column">
                         <textarea
                             type="text"
                             name="description"
@@ -184,27 +182,32 @@ export default class PortfolioForm extends Component {
                             config={this.componentConfig()}
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleThumbDrop()}
-                        />
+                        >
+                            <div className="dz-message">Thumbnail</div>
+                        </DropzoneComponent>
 
                         <DropzoneComponent
                             ref={this.bannerRef}
                             config={this.componentConfig()}
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleBannerDrop()}
-                        />
-                        
+                        >
+                            <div className="dz-message">Banner</div>
+                        </DropzoneComponent>
+
                         <DropzoneComponent
                             ref={this.logoRef}
                             config={this.componentConfig()}
                             djsConfig={this.djsConfig()}
                             eventHandlers={this.handleLogoDrop()}
-                        />
+                        >
+                            <div className="dz-message">Logo</div>
+                        </DropzoneComponent>
                     </div>
                     <div>
-                        <button type="submit">Save</button>
+                        <button className='btn' type="submit">Save</button>
                     </div>
                 </form>
-            </div>
         );
     }
 }
