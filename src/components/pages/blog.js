d1c1db38790836ef4d69c9c9274d17875f610d4a
gleekzorp <gleek.dan@gmail.com>
2019-03-10 19:39:44 -0600

    Blog Memory Leak Fixed

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -16,25 +16,24 @@ class Blog extends Component {
         }
 
         this.getBlogItems = this.getBlogItems.bind(this);
-        this.activateInfiniteScroll();
+        this.onScroll = this.onScroll.bind(this);
+        window.addEventListener("scroll", this.onScroll, false);
     }
 
-    activateInfiniteScroll() {
-        window.onscroll = () => {
-            if (
-                this.state.isLoading ||
-                this.state.blogItems.length === this.state.totalCount
-            ) {
-                return;
-            }
-            
-            if (
-                window.innerHeight + document.documentElement.scrollTop ===
-                document.documentElement.offsetHeight
-            ) {
-                this.getBlogItems();
-            }
-        };
+    onScroll() {
+        if (
+            this.state.isLoading ||
+            this.state.blogItems.length === this.state.totalCount
+        ) {
+            return;
+        }
+
+        if (
+            window.innerHeight + document.documentElement.scrollTop ===
+            document.documentElement.offsetHeight
+        ) {
+            this.getBlogItems();
+        }
     }
 
     getBlogItems() {
@@ -61,6 +60,10 @@ class Blog extends Component {
 
     componentWillMount() {
         this.getBlogItems();
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false);
     }
 
     render() {
