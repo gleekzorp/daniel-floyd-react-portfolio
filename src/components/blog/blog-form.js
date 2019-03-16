92d7f1029c588456dfb91547b87bd0b57f83166a
gleekzorp <gleek.dan@gmail.com>
2019-03-15 19:03:04 -0600

    Up to and finished Building the styles for the new blog icon with a fixed position on the page

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -13,8 +14,36 @@ export default class BlogForm extends Component {
         this.handleSubmit = this.handleSubmit.bind(this)
     }
 
+    buildForm() {
+        let formData = new FormData();
+
+        formData.append("portfolio_blog[title]", this.state.title);
+        formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+
+        return formData;
+    }
+
     handleSubmit(event) {
-        this.props.handleSuccessfullFormSubmission(this.state);
+        axios
+            .post(
+                "https://danielfloyd.devcamp.space/portfolio/portfolio_blogs",
+                this.buildForm(),
+                { withCredentials: true }
+            )
+            .then(response => {
+                this.props.handleSuccessfullFormSubmission(
+                    response.data.portfolio_blog
+                );
+
+                this.setState({
+                    title: "",
+                    blog_status: ""
+                });
+            })
+            .catch(error => {
+                console.log("handleSubmit for blog error", error)
+            });
+
         event.preventDefault();
     }
 
@@ -25,7 +54,8 @@ export default class BlogForm extends Component {
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} className="blog-form-wrapper">
+                <div className="two-column">
                 <input
                     type="text"
                     onChange={this.handleChange}
@@ -40,8 +70,8 @@ export default class BlogForm extends Component {
                     placeholder="Blog Status"
                     value={this.state.blog_status}
                 />
-
-                <button>Save</button>
+                </div>
+                <button className="btn">Save</button>
             </form>
         );
     }
